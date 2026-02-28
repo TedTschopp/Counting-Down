0dc842c8a9fb98bd40934bc6e03df0113384c2f3
Ted Tschopp <ted@tschopp.org>
2026-02-28 14:38:01 -0800

    Initial Commit

---
+++ Counting Down/Counting_DownApp.swift
@@ -0,0 +1,32 @@
+//
+//  Counting_DownApp.swift
+//  Counting Down
+//
+//  Created by Ted Tschopp on 2/28/26.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Counting_DownApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Counting DownTests/Counting_DownTests.swift
@@ -0,0 +1,16 @@
+//
+//  Counting_DownTests.swift
+//  Counting DownTests
+//
+//  Created by Ted Tschopp on 2/28/26.
+//
+
+import Testing
+
+struct Counting_DownTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
